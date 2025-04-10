d4959301ba1ba2f729c6fcda9675341eca767f44
VuManh2403 <vdm24032002@gmail.com>
2025-04-10 17:20:07 +0700

    database

--- src/main/java/com/example/webbansach_backend/entity/HinhAnh.java
+++ src/main/java/com/example/webbansach_backend/entity/HinhAnh.java
@@ -1,18 +1,32 @@
 package com.example.webbansach_backend.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
 @Data
+@Table(name = "hinh_anh")
 public class HinhAnh {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ma_hinh_anh")
     private int maHinhAnh;
 
+    @Column(name = "ten_hinh_anh", length = 256)
     private String tenHinhAnh;
 
+    @Column(name = "la_icon")
     private boolean laIcon;
 
+    @Column(name = "duong_dan")
     private String duongDan;
 
+    @Column(name = "du_lieu_anh")
     private String duLieuAnh;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
     private Sach sach;
 }
--- src/main/java/com/example/webbansach_backend/entity/Sach.java
+++ src/main/java/com/example/webbansach_backend/entity/Sach.java
@@ -1,37 +1,72 @@
 package com.example.webbansach_backend.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
 
+@Entity
 @Data
+@Table(name = "sach")
 public class Sach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ma_sach")
     private int maSach;
 
+    @Column(name = "ten_sach",length = 256)
     private String tenSach;
 
+    @Column(name = "ten_tac_gia", length = 512)
     private String tenTacGia;
 
     // ISBN ma so xuat ban the gioi
+    @Column(name = "isbn",length = 256)
     private String ISBN;
 
+    @Column(name = "mo_ta", columnDefinition = "TEXT")
     private String moTa;
 
+    @Column(name = "gia_niem_yet")
     private double giaNiemYet;
 
+    @Column(name = "gia_ban")
     private double giaBan;
 
+    @Column(name = "so_luong")
     private int soLuong;
 
+    @Column(name = "trung_binh_xep_hang")
     private Double trungBinhXepHang;
 
+    @ManyToMany(fetch = FetchType.LAZY, // lazy chỉ được tải khi bạn gọi đến nó (lazy loading). Tối ưu hiệu năng khi không cần luôn luôn tải kèm dữ liệu liên quan.
+            cascade = { // Chỉ định những hành động nào sẽ tự động áp dụng từ Sach sang TheLoai
+                    CascadeType.PERSIST, //Khi lưu Sach, tự động lưu TheLoai nếu chưa có
+                    CascadeType.MERGE, //Khi cập nhật Sach, tự động cập nhật TheLoai
+                    CascadeType.REFRESH, //Khi làm mới Sach, cũng làm mới TheLoai
+                    CascadeType.DETACH //Khi tách Sach khỏi EntityManager, TheLoai cũng bị tách theo
+            })
+    @JoinTable( // tên bảng trung gian
+            name = "sach_theloai",
+            joinColumns = @JoinColumn(name = "ma_sach"), // cot dai dien cho entity hien tai
+            inverseJoinColumns = @JoinColumn(name = "ma_the_loai") // cot dai dien cho entity con lai
+    )
     List<TheLoai> danhSachTheLoai;
 
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     List<HinhAnh> danhSachHinhAnh;
 
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     List<SuDanhGia> danhSachSuDanhGia;
 
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.DETACH
+    })
     List<ChiTietDonHang> danhSachChiTietDonHang;
 
+    @OneToMany(mappedBy = "sach", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     List<SachYeuThich> danhSachSachYeuThich;
 }
